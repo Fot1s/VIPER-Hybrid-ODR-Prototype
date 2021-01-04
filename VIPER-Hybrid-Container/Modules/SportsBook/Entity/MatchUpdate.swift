fd2b39f47d12049122156448a95a62ed749d2507
Fotis Chatzinikos <fotis.chatzinikos@gmail.com>
2021-01-04 21:54:26 +0200

    MatchUpdate provides now an Extension to Match to get or set the bet value via the MatchUpdate

--- VIPER-Hybrid-Container/Modules/SportsBook/Entity/MatchUpdate.swift
+++ VIPER-Hybrid-Container/Modules/SportsBook/Entity/MatchUpdate.swift
@@ -12,14 +12,39 @@ import Foundation
 
 struct MatchUpdate:Codable {
 
-    enum UpdateFor: Int,Codable {
+    enum BetType: Int,Codable {
         case Home = 1
         case Draw = 2
         case Away = 3
     }
 
     var id = 0
-    var updateFor = UpdateFor.Draw
+    var updateFor = BetType.Draw
     var value = 0
 }
 
+extension Match {
+    
+    mutating func updateMatchBetFromMatchUpdate(_ updatedMatch:MatchUpdate) {
+        switch (updatedMatch.updateFor) {
+        case .Home :
+            self.bet1 = updatedMatch.value
+        case .Draw :
+            self.betX = updatedMatch.value
+        case .Away :
+            self.bet2 = updatedMatch.value
+        }
+    }
+    
+    func getBetValueForBetType(_ update:MatchUpdate.BetType) -> Int {
+        switch update {
+        case .Home :
+            return self.bet1
+        case .Draw :
+            return self.betX
+        case .Away :
+            return self.bet2
+        }
+    }
+
+}
