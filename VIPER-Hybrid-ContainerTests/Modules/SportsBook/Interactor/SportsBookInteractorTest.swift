f28704cb09bca1cd77cb50782ab4f7e977bad6d3
Fotis Chatzinikos <fotis.chatzinikos@gmail.com>
2021-01-04 20:06:47 +0200

    Cleaned up SportsBookInteractorTests, removed failing tests class - added all tests in one class

--- VIPER-Hybrid-ContainerTests/Modules/SportsBook/Interactor/SportsBookInteractorTest.swift
+++ VIPER-Hybrid-ContainerTests/Modules/SportsBook/Interactor/SportsBookInteractorTest.swift
@@ -35,6 +35,13 @@ class SportsBookInteractorTest: XCTestCase {
         super.tearDown()
     }
     
+    func testFetchMatchesFails() {
+        MockAPIService.shared.failOnFetchMatches = true
+        sut?.fetchMatches()
+        
+        XCTAssertNotNil(mockOutput?.matchesFailedWithError, "matchesFailedWithError should not be nil")
+    }
+    
     func testFetchMatches() {
         
         //fetch matches - MockAPIService will return 1 match
@@ -57,32 +64,46 @@ class SportsBookInteractorTest: XCTestCase {
         XCTAssertTrue(match.bet2 == 300)
     }
     
-    func testConnectToSocketSendAndReceiveAMatchUpdate() {
+    func testConnectToSocketServerFails() {
         
+        MockWebSocketService.shared.failToConnect = true 
+        sut?.connectToSocketServerForUpdates()
+
+        XCTAssertNotNil(mockOutput?.socketConnected)
+        XCTAssertFalse(mockOutput?.socketConnected ?? true)
+    }
+    
+    func testConnectAndDisconnectFromSocketServer() {
         //test connection
         sut?.connectToSocketServerForUpdates()
         
-        XCTAssertNotNil(mockOutput!.socketConnected)
-        XCTAssertTrue(mockOutput!.socketConnected!)
+        XCTAssertNotNil(mockOutput?.socketConnected)
+        XCTAssertTrue(mockOutput?.socketConnected ?? false)
+        
+        //test disconnection
+        sut?.disconnectFromSocketServer()
+        
+        XCTAssertFalse(mockOutput?.socketConnected ?? true)
+    }
+    
+    func testConnectToSocketSendAndReceiveAMatchUpdate() {
+        
+        //connection
+        sut?.connectToSocketServerForUpdates()
         
         //test an update
         let matchToUpdate = MatchUpdate(id: 1, updateFor: .Draw, value: 250)
         
-        //sut?.fakeUpdateSend(matchToUpdate:matchToUpdate)
         MockWebSocketService.shared.fakeUpdateSend(matchToUpdate:matchToUpdate)
-        XCTAssertNotNil(mockOutput!.updatedMatch)
+        XCTAssertNotNil(mockOutput?.updatedMatch)
         
-        let updatedMatch = mockOutput!.updatedMatch
+        let updatedMatch = mockOutput?.updatedMatch
         
         XCTAssertTrue(matchToUpdate.id == updatedMatch?.id)
         XCTAssertTrue(matchToUpdate.updateFor == updatedMatch?.updateFor)
         XCTAssertTrue(matchToUpdate.value == updatedMatch?.value)
-
-        //test disconnection 
-        sut?.disconnectFromSocketServer()
-        
-        XCTAssertFalse(mockOutput!.socketConnected!)
     }
+    
 }
 
 class MockSportsBookInteractorOutput: SportsBookInteractorOutput {
@@ -90,13 +111,14 @@ class MockSportsBookInteractorOutput: SportsBookInteractorOutput {
     var matches:[Match]?
     var socketConnected:Bool?
     var updatedMatch:MatchUpdate?
+    var matchesFailedWithError:String?
     
     func matchesFetched(_ matches: [Match]) {
         self.matches = matches
     }
     
     func matchesFetchFailed(_ error: String) {
-        
+        self.matchesFailedWithError = error
     }
     
     func connectedToSocketServer() {
@@ -118,6 +140,8 @@ class MockWebSocketService: ViperWebSocket {
     
     var websocket: WebSocket!
     
+    var failToConnect = false
+    
     private init() {
         let url = URL(string: "ws://none")!
         let request = URLRequest(url: url)
@@ -127,13 +151,18 @@ class MockWebSocketService: ViperWebSocket {
     
     func connect(withDelegate delegate:WebSocketDelegate) {
         websocket.delegate = delegate
-        websocket.delegate?.websocketDidConnect(socket: websocket)
         
-        //Just for test coverage:
-        //send an empty Data
-        websocket.delegate?.websocketDidReceiveData(socket: websocket, data: Data())
-        //send a broken json text
-        websocket.delegate?.websocketDidReceiveMessage(socket: websocket, text: "broken")
+        if (failToConnect) {
+            websocket.delegate?.websocketDidDisconnect(socket: websocket, error: nil)
+        } else {
+            websocket.delegate?.websocketDidConnect(socket: websocket)
+            
+            //Just for test coverage:
+            //send an empty Data
+            websocket.delegate?.websocketDidReceiveData(socket: websocket, data: Data())
+            //send a broken json text
+            websocket.delegate?.websocketDidReceiveMessage(socket: websocket, text: "broken")
+        }
     }
     
     func write(message: String) {
@@ -165,15 +194,20 @@ class MockAPIService: ViperNetwork {
 
     static let shared = MockAPIService()
 
+    var failOnFetchMatches = false
+    
     private init() {
 
     }
 
     //Generic version of fetch no need to have multiple versions as bellow
     func fetch<T>(endPointURL: String, completion: @escaping ([T]?) -> Void) where T:Codable {
-
-        let matches:[Match]? = [Match(id:1, live:1, time:300, date:"n/a", home:"home", away:"away", homeGoals:0,awayGoals:0, bet1:100,betX:200,bet2:300)]
-        completion(matches as? [T])
+        if (failOnFetchMatches) {
+            completion(nil)
+        } else {
+            let matches:[Match]? = [Match(id:1, live:1, time:300, date:"n/a", home:"home", away:"away", homeGoals:0,awayGoals:0, bet1:100,betX:200,bet2:300)]
+            completion(matches as? [T])
+        }
     }
 }
 
