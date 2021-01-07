2e31d92a38587eb03831cc8ef4a185f562cadb87
Fotis Chatzinikos <fotis.chatzinikos@gmail.com>
2021-01-07 22:56:39 +0200

    Added variations for the intro screen (in code) depending on the orientation. The stack view now has a vertical orientation for portrait modes and horizontal for landscape modes

--- VIPER-Hybrid-Container/Modules/Intro/View/IntroViewController.swift
+++ VIPER-Hybrid-Container/Modules/Intro/View/IntroViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class IntroViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var topImage: UIImageView!
+    @IBOutlet weak var middleImage: UIImageView!
+    @IBOutlet weak var bottomImage: UIImageView!
+
     @IBAction func showWebAndODRContentAction(_ sender: Any) {
         presenter.didSelectWebAndODRContent()
     }
@@ -24,13 +29,34 @@ class IntroViewController: UIViewController {
 
     var presenter: IntroPresentation!
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        changeViewDirectionAndImageContentModes()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
 
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+
+        changeViewDirectionAndImageContentModes()
+    }
+
     fileprivate func setupView() {
         navigationItem.title = Localization.Intro.navigationBarTitle
+    }
+
+    //change stackview direction and image content modes depending on orientation
+    func changeViewDirectionAndImageContentModes() {
+        if UIDevice.current.orientation.isPortrait {
+            stackView.axis = .vertical
+        } else {
+            stackView.axis = .horizontal
+        }
     }
 }
 
